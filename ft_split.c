1123887ce0556f178b83563841c56058e3ecb18b
Hugo Brygo <hubrygo@fu-r2-p8.s19.be>
2023-02-22 09:30:10 +0100

    third commit split

--- ft_split.c
+++ ft_split.c
@@ -9,8 +9,8 @@
 /*   Updated: 2023/02/20 16:26:42 by hubrygo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int	next_sep(char *str, char *charset)
 {
@@ -57,7 +57,7 @@ int	ft_strlen(char *str, int i)
 	return (i);
 }
 
-int num_of_sep(char *str, char *charset)
+int	num_of_sep(char *str, char *charset)
 {
 	int	i;
 	int	j;
@@ -80,81 +80,85 @@ int num_of_sep(char *str, char *charset)
 	}
 	return (count);
 }
-//i[0] = sep
-//i[1] = i
-//i[2] = j
-//i[3] = len
 
-char **ft_split(char *str, char *charset)
+// char **ft_split(char *str, char *charset)
+// {
+// 	int i[4];
+// 	char **tab;
+// 	int count;
+
+// 	count = 0;
+// 	i[2] = 0;
+// 	i[3] = 0;
+// 	i[0] = num_of_sep(str, charset);
+// 	tab = malloc(sizeof(char *) * (i[0] + 1));
+// 	if (!tab)
+// 		return (NULL);
+// 	while (i[2] < ft_strlen(str, -1))
+// 	{
+// 		i[1] = i[2];
+// 		i[2] = next_sep(str, charset);
+// 		if (i[2] == 0)
+// 			return (tab);
+// 		tab[i[3]] = ft_strdup(str, i[1], i[2]);
+// 		i[3]++;
+// 		i[2]++;
+// 	}
+// 	return (tab);
+// }
+
+//a[0] = i
+//a[1] = sep
+//a[2] = j
+//a[3] = len
+//a[4] = bool
+//a[5] = a
+char	**ft_split(char *str, char *charset)
 {
-	int	i[4];
 	char	**tab;
-	int	count;
+	int		a[6];
 
-	count = 0;
-	i[2] = 0;
-	i[3] = 0;
-	i[0] = num_of_sep(str, charset);
-	tab = malloc(sizeof(char *) * (i[0] + 1));
-	if(!tab)
-		return (NULL);
-	while (i[2] < ft_strlen(str, -1))
-	{
-		i[1] = i[2];
-		i[2] = next_sep(str, charset);
-		if (i[2] == 0)
-			return (tab);
-		tab[i[3]] = ft_strdup(str, i[1], i[2]);
-		i[3]++;
-		i[2]++;
-	}
-	return (tab);
-}
-
-/*char **ft_split(char *str, char *charset)
-{
-	int	sep;
-	char	**tab;
-	int	i;
-	int	j;
-	int	len;
-
-	i = 0;
-	len = 0;
-	sep = num_of_sep(str, charset);
-	tab = malloc(sizeof(char) * (sep + 1));
+	a[0] = 0;
+	a[5] = 0;
+	a[4] = 0;
+	a[3] = 0;
+	a[1] = num_of_sep(str, charset);
+	tab = malloc(sizeof(char *) * (a[1] + 2));
 	if (!tab)
 		return (NULL);
-	while (str[i])
+	while (str[a[0]])
 	{
-		j = 0;
-		while (str[len + j] == charset[j] && str[len + j] && charset[j])
-			j++;
-		if (j == (ft_strlen(charset)))
+		a[2] = -1;
+		while (charset[++a[2]])
+			if (str[a[0]] == charset[a[2]])
+				a[4] = 1;
+		if (a[4])
 		{
-			tab[i] = ft_strdup(&str[i], len);
-			len += j;
-			i +=len + 1;
+			if (a[3] <= 0)
+				tab[a[5]] = ft_strdup(&str[0], 0, a[0]);
+			else
+				tab[a[5]] = ft_strdup(&str[a[3]], a[3], a[0]);
+			a[5]++;
+			a[3] = a[0] + 1;
 		}
-		//printf("%d\n", len);
-		//printf("%d", i);
-		len++;
-
+		a[0]++;
+		a[4] = 0;
 	}
+	tab[a[5]] = ft_strdup(&str[a[3]], a[3], a[0]);
 	return (tab);
 }
-*/
+
 #include <stdio.h>
-int	main()
-{	
+int main(int argc, char **argv)
+{
 	int i = 0;
 	char *str = "beep,boop, oi,he4ll";
-	char *charset = " ";
+	char *charset = ",";
 	char **tab;
 	tab = ft_split(str, charset);
 	while (tab[i])
 	{
-		printf("%s\n",tab[i]);
+		printf("%s\n", tab[i]);
 		i++;
 	}
 }
